a24e6219803b8a9b5108e0cc47645dc3e48c94a4
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 18:48:46 -0300

    feat: middleware of errors

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { routes } from "../router/routes";
+import { errorMiddleware } from "../middleware/Error.Middleware";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errorMiddleware);
 
 export { app };

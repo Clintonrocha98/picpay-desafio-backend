beaf24d5a402286656be21a5dc7894e1153a2d44
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 18:51:38 -0300

    feat: route /transaction create

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,15 @@
 import { Request, Response, Router } from "express";
 import { userFactory } from "../factory/User/User.factory";
+import { transactionFactory } from "../factory/Transaction/Transaction.Factory";
 
 const routes = Router();
 
-routes.get("/", (req: Request, res: Response) => {
-  res.status(200).json({ hello: "world" });
-});
 routes.post("/user", (req: Request, res: Response) =>
   userFactory().newUser(req, res)
+);
+
+routes.post("/transaction", (req: Request, res: Response) =>
+  transactionFactory().saveTransaction(req, res)
 );
 
 export { routes };

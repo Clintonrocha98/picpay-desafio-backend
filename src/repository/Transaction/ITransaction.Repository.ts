8fded493fe95feb3913d4a8611565f1db8d76d78
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 18:54:50 -0300

    feat: created authorization service interface and update the others

--- src/repository/Transaction/ITransaction.Repository.ts
+++ src/repository/Transaction/ITransaction.Repository.ts
@@ -2,6 +2,6 @@ import { Transaction } from "../../models/Transaction/Transaction";
 
 export interface ITransactionRepository {
   saveTransaction(data: Transaction): Promise<Transaction>;
-  receiveUpdatebalance(recipient_id: number, amount: number): Promise<void>;
-  senderUpdatebalance(sender_id: number, amount: number): Promise<void>;
+  payerUpdatebalance(payer_id: number, amount: number): Promise<void>;
+  payeeUpdatebalance(payee_id: number, amount: number): Promise<void>;
 }
--- src/repository/User/IUser.Repository.ts
+++ src/repository/User/IUser.Repository.ts
@@ -2,8 +2,7 @@ import { User } from "../../models/user/User";
 
 export interface IUserRepository {
   newUser(user: User): Promise<User>;
-  userById(id: number): Promise<User | []>;
-  userExist(id: number): Promise<boolean>;
-  userByEmail(email: string): Promise<User>;
-  users(): Promise<User[]>;
+  userById(id: number): Promise<User>;
+  emailExist(email: string): Promise<boolean>;
+  documentExist(document: string): Promise<boolean>;
 }

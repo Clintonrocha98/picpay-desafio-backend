862795ec9f518a452e9414007a9367bc9cc3e25d
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 19:05:21 -0300

    feat: created ExternalAuthorizationService

---
+++ src/service/Transaction/ExternalAuthorization/ExternalAuthorization.Service.ts
@@ -0,0 +1,12 @@
+import { AuthorizationService } from "./IAuthorization.Service";
+
+export class ExternalAuthorizationService implements AuthorizationService {
+  async authorizeTransaction(): Promise<boolean> {
+    const response = await fetch(
+      "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc"
+    );
+    const data = await response.json();
+    
+    return data.message === "Autorizado";
+  }
+}

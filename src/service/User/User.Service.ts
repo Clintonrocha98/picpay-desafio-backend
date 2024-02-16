77cace35f50cfbc76bd83ee1d7a1ce60d63ad6bd
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 18:59:21 -0300

    feat: remove method userbyid and add custom erros on newUser method

--- src/service/User/User.Service.ts
+++ src/service/User/User.Service.ts
@@ -1,20 +1,25 @@
 import { User } from "../../models/user/User";
 import { IUserRepository } from "../../repository/User/IUser.Repository";
+import { InvalidDocument, InvalidEmail } from "./Error/User.Error";
 
 export class UserService {
   constructor(private userRepository: IUserRepository) {}
 
   async newUser(user: User) {
+    const EmailExist = await this.userRepository.emailExist(user.email);
+
+    if (!EmailExist) {
+      throw new InvalidEmail("Email invalido");
+    }
+
+    const DocumentExist = await this.userRepository.documentExist(
+      user.document
+    );
+
+    if (!DocumentExist) {
+      throw new InvalidDocument("Document invalid");
+    }
     const newUser = await this.userRepository.newUser(user);
     return newUser;
-  }
-
-  async userById(id: number) {
-    const user = await this.userRepository.userById(id);
-    return user;
-  }
-  async users() {
-    const user = await this.userRepository.users();
-    return user;
   }
 }

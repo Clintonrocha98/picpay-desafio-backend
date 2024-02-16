75ab92bdba8a0d531d08bfe130042ed032b161d8
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 19:04:20 -0300

    feat: adition of the authorizeTransaction in the transaction service

--- src/service/Transaction/Transaction.Service.ts
+++ src/service/Transaction/Transaction.Service.ts
@@ -1,35 +1,63 @@
 import { Transaction } from "../../models/Transaction/Transaction";
 import { ITransactionRepository } from "../../repository/Transaction/ITransaction.Repository";
 import { IUserRepository } from "../../repository/User/IUser.Repository";
+import {
+  PayeeInvalid,
+  PayerDoesNotHaveSufficientBalance,
+  PayerInvalid,
+  RetailerCannotMakeTransfer,
+  UnauthorizedTransaction,
+} from "./Error/Transaction.Error";
+import { ExternalAuthorizationService } from "./ExternalAuthorization/ExternalAuthorization.Service";
+import { AuthorizationService } from "./ExternalAuthorization/IAuthorization.Service";
 
 export class TransactionService {
+  private authorizationService: AuthorizationService;
+
   constructor(
     private transactionRepository: ITransactionRepository,
-    private userRepository: IUserRepository
-  ) {}
-  
-  async saveTransaction({ amount, sender, receiver, timestamp }: Transaction) {
-    const userSender = await this.userRepository.userById(Number(sender));
-    const userReceiver = await this.userRepository.userById(Number(receiver));
+    private userRepository: IUserRepository,
+    authorizationService: AuthorizationService = new ExternalAuthorizationService()
+  ) {
+    this.authorizationService = authorizationService;
+  }
 
-    if (!userSender) {
-      throw new Error("Remetente invalido");
+  async saveTransaction({ amount, payer, payee }: Transaction) {
+    const userPayer = await this.userRepository.userById(Number(payer));
+    const userPayee = await this.userRepository.userById(Number(payee));
+
+    if (!userPayer) {
+      throw new PayerInvalid("Pagador invalido");
     }
-    if (userSender["userType"] === "lojista") {
-      throw new Error("Lojista não pode fazer transferencia");
+    
+    if (userPayer.usertype === "lojista") {
+      throw new RetailerCannotMakeTransfer(
+        "Lojista não pode fazer transferencia"
+      );
     }
-    if (!userReceiver) {
-      throw new Error("Destinatário invalido");
+    if (!userPayee) {
+      throw new PayeeInvalid("Destinatário invalido");
     }
-    if (userSender["balance"] < amount) {
-      throw new Error("Remetente não possui saldo suficiente");
+    if (userPayer.balance < amount) {
+      throw new PayerDoesNotHaveSufficientBalance(
+        "Remetente não possui saldo suficiente"
+      );
     }
+
+    const isAuthorized = await this.authorizationService.authorizeTransaction();
+
+    if (!isAuthorized) {
+      throw new UnauthorizedTransaction("Transação não autorizada");
+    }
+
+    await this.transactionRepository.payeeUpdatebalance(Number(payee), amount);
+    await this.transactionRepository.payerUpdatebalance(Number(payer), amount);
 
     const transaction = await this.transactionRepository.saveTransaction({
       amount,
-      sender,
-      receiver,
-      timestamp,
+      payer,
+      payee,
+      date_transaction: new Date(),
     });
 
     return transaction;

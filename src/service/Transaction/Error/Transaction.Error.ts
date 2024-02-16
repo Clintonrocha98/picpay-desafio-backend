f957855138ce1fb34abf5c720030f6cd1146cdf6
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-16 19:04:52 -0300

    feat: created custom erros  of transactions

---
+++ src/service/Transaction/Error/Transaction.Error.ts
@@ -0,0 +1,28 @@
+import { ApiError } from "../../../helpers/Api.Errors";
+
+export class PayerInvalid extends ApiError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+export class PayeeInvalid extends ApiError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+export class RetailerCannotMakeTransfer extends ApiError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
+
+export class UnauthorizedTransaction extends ApiError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
+export class PayerDoesNotHaveSufficientBalance extends ApiError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}

1db4402083fa960898510e538374c452a168ca3e
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-17 19:38:28 -0300

    test: create tests user service

---
+++ src/service/User/user.service.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect, describe } from "vitest";
+import { UserService } from "./User.Service";
+import { User } from "../../models/user/User";
+import { InMemoryUserRepository } from "../../repository/User/InMemory/User.Repository.InMemory";
+
+const makeSut = () => {
+  const inMemoryUserRepository = new InMemoryUserRepository();
+  const userService = new UserService(inMemoryUserRepository);
+  const fakeUser: User = {
+    fristName: "fulano",
+    lastName: "de tal",
+    document: "12345678901",
+    balance: 1000,
+    email: "fulano@email.com",
+    password: "123456789",
+    usertype: "comum",
+  };
+
+  return { userService, fakeUser };
+};
+
+describe("User service", () => {
+  test("deve ser possivel criar um usuario", async () => {
+    const { fakeUser, userService } = makeSut();
+    const user = await userService.newUser(fakeUser);
+
+    expect(user).toHaveProperty("id");
+  });
+  test("Não deve ser possivel criar um usuario com email que já existe", async () => {
+    const { fakeUser, userService } = makeSut();
+    await userService.newUser(fakeUser);
+
+    await expect(userService.newUser(fakeUser)).rejects.toThrow(
+      "Email invalido"
+    );
+  });
+  test("Não deve ser possivel criar um usuario com documento que já existe", async () => {
+    const { fakeUser, userService } = makeSut();
+    await userService.newUser({
+      ...fakeUser,
+      email: "emaildiferente@email.com",
+    });
+
+    await expect(userService.newUser(fakeUser)).rejects.toThrow(
+      "Document invalid"
+    );
+  });
+});

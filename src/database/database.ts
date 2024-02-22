18c7e559062da60c74d351751e8adab743ab1b88
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-22 17:04:25 -0300

    remove coment lines

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,27 +1,6 @@
-import pg, { Pool } from "pg";
+import pg from "pg";
 
-let pool: Pool;
-
-// if (process.env.NODE_ENV === "test") {
-//   console.log("entrou aqui");
-//   pool = new pg.Pool({
-//     host: process.env.TEST_POSTGRE_HOST,
-//     port: 5432,
-//     user: process.env.TEST_POSTGRE_USER,
-//     password: process.env.TEST_POSTGRE_PASSWORD,
-//     database: process.env.TEST_POSTGRE_DATABASE,
-//   });
-// } else {
-//   console.log("aaa");
-//   pool = new pg.Pool({
-//     host: process.env.POSTGRE_HOST,
-//     port: 5432,
-//     user: process.env.POSTGRE_USER,
-//     password: process.env.POSTGRE_PASSWORD,
-//     database: process.env.POSTGRE_DATABASE,
-//   });
-// }
-pool = new pg.Pool({
+const pool = new pg.Pool({
   host: process.env.TEST_POSTGRE_HOST || process.env.POSTGRE_HOST,
   port: 5432,
   user: process.env.TEST_POSTGRE_USER || process.env.POSTGRE_USER,

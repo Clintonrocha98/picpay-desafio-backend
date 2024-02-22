2e2839968c3d530f9c76fda898e094d47dd90792
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-22 20:54:34 -0300

    feat: change and added import for factory transaction

--- src/factory/Transaction/Transaction.Factory.ts
+++ src/factory/Transaction/Transaction.Factory.ts
@@ -4,15 +4,17 @@ import { TransactionService } from "../../service/Transaction/transaction.servic
 import { TransactionController } from "../../controller/Transaction/transaction.controller";
 import { ExternalAuthorizationService } from "../../service/ExternalAuthorization/externalAuthorization.service";
 import { ExternalNotificationService } from "../../service/ExternalNotification/externalNotification.service";
+import { UserService } from "../../service/User/user.service";
 
 export const transactionFactory = () => {
   const transactionRepository = new TransactionRepository();
   const userRepository = new UserRepository();
+  const userService = new UserService(userRepository);
   const authorizationService = new ExternalAuthorizationService();
   const notificationService = new ExternalNotificationService();
   const transactionService = new TransactionService(
     transactionRepository,
-    userRepository,
+    userService,
     authorizationService,
     notificationService
   );

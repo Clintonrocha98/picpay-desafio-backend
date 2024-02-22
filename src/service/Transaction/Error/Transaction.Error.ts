a2e39c8489889c93bd30157a54c522dde3c38c5c
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-22 20:51:55 -0300

    update: remove erros

--- src/service/Transaction/Error/Transaction.Error.ts
+++ src/service/Transaction/Error/Transaction.Error.ts
@@ -1,28 +1,7 @@
 import { ApiError } from "../../../helpers/api.errors";
 
-export class PayerInvalid extends ApiError {
-  constructor(message: string) {
-    super(message, 404);
-  }
-}
-export class PayeeInvalid extends ApiError {
-  constructor(message: string) {
-    super(message, 404);
-  }
-}
-export class RetailerCannotMakeTransfer extends ApiError {
-  constructor(message: string) {
-    super(message, 403);
-  }
-}
-
 export class UnauthorizedTransaction extends ApiError {
   constructor(message: string) {
     super(message, 401);
-  }
-}
-export class PayerDoesNotHaveSufficientBalance extends ApiError {
-  constructor(message: string) {
-    super(message, 403);
   }
 }

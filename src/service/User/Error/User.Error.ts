7e2de4c32cab824318d7ecbeda75feeda3c01f45
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-02-22 20:51:35 -0300

    update: change local of the errors

--- src/service/User/Error/User.Error.ts
+++ src/service/User/Error/User.Error.ts
@@ -11,3 +11,19 @@ export class InvalidDocument extends ApiError {
     super(message, 401);
   }
 }
+export class InvalidUser extends ApiError {
+  constructor(message: string) {
+    super(message, 401);
+  }
+}
+
+export class PayerDoesNotHaveSufficientBalance extends ApiError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
+export class RetailerCannotMakeTransfer extends ApiError {
+  constructor(message: string) {
+    super(message, 403);
+  }
+}
